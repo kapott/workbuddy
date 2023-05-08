04b521064ca9b983a71a6db18ffb99f327c34716
kapott <kapott@aivd.33mail.com>
2023-05-08 13:15:12 +0200

    xrandr setting scripts added.

---
+++ roles/i3wm/files/scripts/home.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# generated line by arandr!
+# 4K HDMI-0 to the right of laptop screen:
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --mode 3840x2160 --pos 2560x0 --rotate normal --output D    ↪ P-4 --mode 2560x1600 --pos 0x280 --rotate normal --output None-1-1 --off
+
---
+++ roles/i3wm/files/scripts/laptop-screen-only.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-0 --off --output DP-4 --primary --mode 2560x1600 --pos 0x0    ↪  --rotate normal --output None-1-1 --off
