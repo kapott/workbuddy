46041256fe471994273c2ae19d28af23dd4d1f08
kapott <kapott@aivd.33mail.com>
2024-05-28 16:59:13 +0200

    Make run a bit more dynamic.

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
-﻿#!/bin/sh
+﻿#!/usr/bin/env bash
 
-sudo apt update -y
-sudo apt install wget curl ansible -y
+_run() {
+    printf "\n>> %s \n\n" "$1"
+    $1
+}
+
+runIfExists() {
+    if [ -x "$(command -v "$1")" ]; then
+        _run "$2"
+    fi
+}
+
+runIfExists "apt" "sudo apt update -y"
+runIfExists "apt" "sudo apt install wget curl ansible -y"
+runIfExists "yum" "sudo yum update -y"
+runIfExists "yum" "sudo yum install wget curl ansible -y"
 
 ansible-pull -t terminal -U https://github.com/Kapott/workbuddy/local-tools.yml 
